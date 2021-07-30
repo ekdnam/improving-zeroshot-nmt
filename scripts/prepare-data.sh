3b5c8289ed87ce7db8dcfb258cb44ff7d5349643
Aditya Mandke <40426312+ekdnam@users.noreply.github.com>
2021-07-30 18:12:26 +0530

    #1

--- scripts/prepare-data.sh
+++ scripts/prepare-data.sh
@@ -15,36 +15,6 @@ REM_NON_PRINT_CHAR=$SCRIPTS/tokenizer/remove-non-printing-char.perl
 BPEROOT=subword-nmt/subword_nmt
 BPE_TOKENS=40000
 
-URLS=(
-    "http://statmt.org/wmt13/training-parallel-europarl-v7.tgz"
-    "http://statmt.org/wmt13/training-parallel-commoncrawl.tgz"
-    "http://data.statmt.org/wmt17/translation-task/training-parallel-nc-v12.tgz"
-    "http://data.statmt.org/wmt17/translation-task/dev.tgz"
-    "http://statmt.org/wmt14/test-full.tgz"
-)
-FILES=(
-    "training-parallel-europarl-v7.tgz"
-    "training-parallel-commoncrawl.tgz"
-    "training-parallel-nc-v12.tgz"
-    "dev.tgz"
-    "test-full.tgz"
-)
-CORPORA=(
-    "training/europarl-v7.de-en"
-    "commoncrawl.de-en"
-    "training/news-commentary-v12.de-en"
-)
-
-# This will make the dataset compatible to the one used in "Convolutional Sequence to Sequence Learning"
-# https://arxiv.org/abs/1705.03122
-if [ "$1" == "--icml17" ]; then
-    URLS[2]="http://statmt.org/wmt14/training-parallel-nc-v9.tgz"
-    FILES[2]="training-parallel-nc-v9.tgz"
-    CORPORA[2]="training/news-commentary-v9.de-en"
-    OUTDIR=wmt14_en_de
-else
-    OUTDIR=wmt17_en_de
-fi
 
 if [ ! -d "$SCRIPTS" ]; then
     echo "Please set SCRIPTS variable correctly to point to Moses scripts."
@@ -63,38 +33,17 @@ mkdir -p $orig $tmp $prep
 
 cd $orig
 
-for ((i=0;i<${#URLS[@]};++i)); do
-    file=${FILES[i]}
-    if [ -f $file ]; then
-        echo "$file already exists, skipping download"
-    else
-        url=${URLS[i]}
-        wget "$url"
-        if [ -f $file ]; then
-            echo "$url successfully downloaded."
-        else
-            echo "$url not successfully downloaded."
-            exit -1
-        fi
-        if [ ${file: -4} == ".tgz" ]; then
-            tar zxvf $file
-        elif [ ${file: -4} == ".tar" ]; then
-            tar xvf $file
-        fi
-    fi
-done
-cd ..
-
 echo "pre-processing train data..."
+# for l in $src $tgt; do
+#     rm $tmp/train.tags.$lang.tok.$l
+#     for f in "${CORPORA[@]}"; do
+#         cat $orig/$f.$l | \
+#             perl $NORM_PUNC $l | \
+#             perl $REM_NON_PRINT_CHAR | \
+#             perl $TOKENIZER -threads 8 -a -l $l >> $tmp/train.tags.$lang.tok.$l
+#     done
+# done
 for l in $src $tgt; do
-    rm $tmp/train.tags.$lang.tok.$l
-    for f in "${CORPORA[@]}"; do
-        cat $orig/$f.$l | \
-            perl $NORM_PUNC $l | \
-            perl $REM_NON_PRINT_CHAR | \
-            perl $TOKENIZER -threads 8 -a -l $l >> $tmp/train.tags.$lang.tok.$l
-    done
-done
 
 echo "pre-processing test data..."
 for l in $src $tgt; do
